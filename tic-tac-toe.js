60810ab651a2ee7927a5933d25cf4fa97827cfa4
Zijian Wang <zw@zijiandembp.myfiosgateway.com>
2021-08-03 21:36:12 -0400

    added one player mode

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,13 @@ const grid = document.querySelectorAll(".container div");
 const gridArray = Array.from(grid);
 const startBtn = document.querySelector(".game-btn");
 let gameStatus = document.querySelector(".game-status");
+let playerNumDisplay = document.querySelector(".game-player-number");
+
+let players = prompt("How many player?", "1 or 2");
+if (!(players == 1 || players == 2)) {
+    alert(`Invalid number of players. We only supoort 1 or 2 players now.`);
+} 
+playerNumDisplay.textContent = `${players}-player Mode`;
 //global flags
 let nextPlayer = false;
 let GameOver = false;
@@ -27,10 +34,9 @@ function clickEffect(e) {
         return;
     };
     //check if the cell was clicked
-    if (e.target.dataset.index){
-        console.log(e.target.classList);
+    if (e.target.dataset.index) {
         return;
-    }
+    };
     //get index of cell 
     const cellIndex = gridArray.indexOf(e.target);
     //decide current player and display
@@ -41,16 +47,17 @@ function clickEffect(e) {
     e.target.setAttribute("data-index", cellIndex);
     // collect and turn into array of currentplayer index
     let playerCells = Array.from(document.querySelectorAll(`.${currentPlayer}`));
-
     let playerIndexArr = playerCells.map((ele) => Number(ele.getAttribute("data-index")));
 
     //check if current player wins or draw or go on to the next player
-    checkWin(currentPlayer,playerIndexArr);
+
+    robotMove(currentPlayer,playerCells);
+    checkWin(currentPlayer, playerIndexArr);
+    nextPlayer = !nextPlayer;
 }
 
 
-
-function checkWin (currentPlayer, playerIndexArr){
+function checkWin(currentPlayer, playerIndexArr) {
     if (winArr.some(combo => combo.test(playerIndexArr.join("")))) {
         gameStatus.textContent = `The winer is ${currentPlayer}!`;
         GameOver = true;
@@ -64,18 +71,33 @@ function checkWin (currentPlayer, playerIndexArr){
         startBtn.classList.add("shown");
         startBtn.classList.remove("game-btn");
     } else {
-        nextPlayer = !nextPlayer;
-        //add robot function here
+        return;
     }
+}
+
+function robotMove(currentPlayer,playerCells) {
+    if (players == 2) {
+        return 0;
+    };
+    nextPlayer = !nextPlayer;
+    currentPlayer = nextPlayer ? "playerO" : "playerX";
+    let robotCell = Math.floor(Math.random() * 9);
+    if (playerCells.length>4) return;
+    while (grid[robotCell].dataset.index) {
+        robotCell = Math.floor(Math.random() * 9);
+        console.log(grid[robotCell]);
+    }
+    grid[robotCell].classList.add(`${currentPlayer}`);
+    grid[robotCell].dataset.index = robotCell;
 }
 //restart btn->function
 function reStartGame() {
     GameOver = false;
     grid.forEach(ele => ele.className = "");
     grid.forEach(ele => ele.setAttribute("data-index", ""));
+    players = prompt("How many player?", "1 or 2");
+    playerNumDisplay.textContent = `${players}-player Mode`;
 }
-
-
 
 //action play and restart
 grid.forEach(cell => cell.addEventListener("click", clickEffect));
